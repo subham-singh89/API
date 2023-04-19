0340aac76dbb5076c5408c1298cde5c7210ef9d0
subham-singh89 <110325911+subham-singh89@users.noreply.github.com>
2023-04-20 04:36:32 +0530

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#ifdef __cplusplus
+#include <iostream>
+#endif
+int main(int, char *argv[])
+{
+#if __STRICT_ANSI__
+printf("Hello World\n");
+#elif defined(_WIN32)
+PrintWithFormat("Hello World\n");
+#elif defined(__PRINTF_DEPRECATED__)
+fprintf(stdout, "Hello World\n");
+#elif defined(__PRINTF_VECTOR__)
+const char *lines[2] ¼ {"Hello World", NULL};
+printf(lines);
+#elif defined(__cplusplus)
+std::cout << "Hello World" << std::endl;
+#else
+#error No terminal output API found
+#endif
+return 0;
+}
